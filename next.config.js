75167092436ef5a425bfc0118f54d6b7ca86c852
Michael Tyson <michaeltyson@outlook.com>
2021-08-15 10:37:27 -0400

    get netlify build working

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  reactStrictMode: true,
-  webpack: (cfg) => {
-      cfg.module.rules.push(
-          {
-              test: /\.md$/,
-              loader: 'frontmatter-markdown-loader',
-              options: { mode: ['react-component'] }
-          }
-      )
-      return cfg;
-  }
-}
+    reactStrictMode: true,
+    webpack: (cfg) => {
+        cfg.module.rules.push({
+            test: /\.md$/,
+            loader: "frontmatter-markdown-loader",
+            options: { mode: ["react-component"] },
+        });
+        return cfg;
+    },
+};
